7956dd488e12b6a52ece650a557b3fe84192be25
betasecond <nbdhc9039@gmail.com>
2024-01-01 14:43:37 +0800

    plantuml support

--- 复制且输出puml/main0.bat
+++ 复制且输出puml/main0.bat
@@ -7,9 +7,9 @@ echo Current directory: %CD%
 if "%V%" == "" (
     echo %V% is not set, staying in the current directory.
 ) else (
-
+    echo Changing directory to: %V
     cd %V
 )
 
-
+echo Starting tree2puml.exe from Program Files...
 start "" "C:\Program Files\Tree This Folder\tree2puml.exe"
