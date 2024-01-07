b95eaa8d3921b815f075520062875ac423a96ee8
betasecond <nbdhc9039@gmail.com>
2024-01-07 18:08:14 +0800

    Update tree2puml.bat to streamline directory navigation and startup process

--- 生成文件夹结构/tree2puml.bat
+++ 生成文件夹结构/tree2puml.bat
@@ -1,16 +1,4 @@
 @echo off
 SETLOCAL EnableExtensions
-
-echo Running script with debugging information...
-
-echo Current directory: %CD%
-
-if "%V%" == "" (
-    echo %V% is not set, staying in the current directory.
-) else (
-    echo Changing directory to: %V
-    cd %V
-)
-
-echo Starting tree2puml.exe from Program Files...
+cd %V
 start "" "C:\Program Files\Tree This Folder\tree2puml.exe"
