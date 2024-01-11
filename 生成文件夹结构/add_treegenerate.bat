4d79f4ae14024fd104f63315051fd7527d9303d6
H1DDENADM1N <65646535+H1DDENADM1N@users.noreply.github.com>
2024-01-11 17:32:15 +0800

    优化安装路径
    现在所有非remove功能的bat放在C:\Program Files\Tree This Folder\bats

--- 生成文件夹结构/add_treegenerate.bat
+++ 生成文件夹结构/add_treegenerate.bat
@@ -7,14 +7,15 @@ md "C:\Program Files\Tree This Folder"
 :: 复制文件到安装目录
 copy "%~dp0TreeThisFolder.ico" "C:\Program Files\Tree This Folder\TreeThisFolder.ico"
 
-copy "%~dp0treetxt.bat" "C:\Program Files\Tree This Folder\treetxt.bat"
-copy "%~dp0treemd.bat" "C:\Program Files\Tree This Folder\treemd.bat"
-copy "%~dp0tree2puml.bat" "C:\Program Files\Tree This Folder\tree2puml.bat"
-copy "%~dp0treetree.bat" "C:\Program Files\Tree This Folder\treetree.bat"
-copy "%~dp0treeseedir.bat" "C:\Program Files\Tree This Folder\treeseedir.bat"
-copy "%~dp0tree2json.bat" "C:\Program Files\Tree This Folder\tree2json.bat"
+md "C:\Program Files\Tree This Folder\bats\"
+copy "%~dp0treetxt.bat" "C:\Program Files\Tree This Folder\bats\treetxt.bat"
+copy "%~dp0treemd.bat" "C:\Program Files\Tree This Folder\bats\treemd.bat"
+copy "%~dp0tree2puml.bat" "C:\Program Files\Tree This Folder\bats\tree2puml.bat"
+copy "%~dp0treetree.bat" "C:\Program Files\Tree This Folder\bats\treetree.bat"
+copy "%~dp0treeseedir.bat" "C:\Program Files\Tree This Folder\bats\treeseedir.bat"
+copy "%~dp0tree2json.bat" "C:\Program Files\Tree This Folder\bats\tree2json.bat"
 
-md "scripts"
+md "C:\Program Files\Tree This Folder\scrcpts\"
 xcopy /s /e /y "%~dp0..\scrcpts\" "C:\Program Files\Tree This Folder\scrcpts\"
 
 md "C:\Program Files\Tree This Folder\runtime"
--- 生成文件夹结构/remove_treegenerate.bat
+++ 生成文件夹结构/remove_treegenerate.bat
@@ -13,15 +13,18 @@ REG DELETE "HKEY_CLASSES_ROOT\Directory\Background\shell\Item0" /f
 :: 延时
 ping 127.0.0.1 -n 2 >nul
 echo 右键菜单项已删除。
-: 删除安装文件
+:: 删除安装文件
 del /s /q "C:\Program Files\Tree This Folder\TreeThisFolder.ico"
 
-del /s /q "C:\Program Files\Tree This Folder\treetxt.bat"
-del /s /q "C:\Program Files\Tree This Folder\treemd.bat"
-del /s /q "C:\Program Files\Tree This Folder\tree2puml.bat"
-del /s /q "C:\Program Files\Tree This Folder\treetree.bat"
-del /s /q "C:\Program Files\Tree This Folder\treeseedir.bat"
-del /s /q "C:\Program Files\Tree This Folder\tree2json.bat"
+del /s /q "C:\Program Files\Tree This Folder\bats\treetxt.bat"
+del /s /q "C:\Program Files\Tree This Folder\bats\treemd.bat"
+del /s /q "C:\Program Files\Tree This Folder\bats\tree2puml.bat"
+del /s /q "C:\Program Files\Tree This Folder\bats\treetree.bat"
+del /s /q "C:\Program Files\Tree This Folder\bats\treeseedir.bat"
+del /s /q "C:\Program Files\Tree This Folder\bats\tree2json.bat"
+rd "C:\Program Files\Tree This Folder\bats\"
+:: 不要用 rd /s /q "C:\Program Files\Tree This Folder\bats\" ，防止把 复制文件夹结构 功能破坏
+
 
 rd /s /q "C:\Program Files\Tree This Folder\scrcpts\"
 
